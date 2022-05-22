9504e2d37d4b9a3f58033de2899033ec7419cb60
thilan <thilan.k@synergenhealth.com>
2022-05-22 20:32:51 +0530

    add missing dto

---
+++ src/main/java/com/example/demo/dtos/UploadFileDTO.java
@@ -0,0 +1,26 @@
+package com.example.demo.dtos;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class UploadFileDTO {
+
+
+    private String name;
+    private MultipartFile file;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MultipartFile getFile() {
+        return file;
+    }
+
+    public void setFile(MultipartFile file) {
+        this.file = file;
+    }
+}
